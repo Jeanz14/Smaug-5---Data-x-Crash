29ca0194c7563c263a4b1857c7ed83b7f5aa7e10
Vitor Henrique Silva Cezar <vvitorhenrique5@gmail.com>
2026-04-26 17:42:15 -0300

    Movimento do inimigo feito
    inimigo completo tirando o hitstun que é sua propria categoria

--- Smaug - Data Crash/Assets/Scripts/ComportamentoHostil.cs
+++ Smaug - Data Crash/Assets/Scripts/ComportamentoHostil.cs
@@ -11,25 +11,30 @@ public class ComportamentoHostil : MonoBehaviour
         ApanhandoDois = 4,
         Morreu = 5
     }
-    [SerializeField] private float velocidade = 3f;
+    [SerializeField] private float velocidade = 5f;
     [SerializeField] private float direcaoAtual = 1f;
+    [SerializeField] private Vector2 alvo;
     [SerializeField] private int dano = 10;
     [SerializeField] private bool podeAtacar = true;
     [SerializeField] private Collider2D hitbox;
     private Animator anim;
+    private Transform posPlayer;
+    private SpriteRenderer sr;
     void Awake()
     {
         anim = GetComponent<Animator>();
+        posPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+        sr = GetComponent<SpriteRenderer>();
     }
     void OnTriggerEnter2D(Collider2D other)
     {
         if(!other.CompareTag("Player")) return;
         if (hitbox.IsTouching(other))
         {
-            
             Debug.Log("Player agrou o inimigo");
             podeAtacar = true;
             anim.SetInteger("IFState", (int)InimigoState.Atacando);
+            alvo = transform.position;
         }
     }
     void OnTriggerExit2D(Collider2D other)
@@ -56,6 +61,23 @@ public class ComportamentoHostil : MonoBehaviour
     }
     void Andar()
     {
-        return;
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        if (!stateInfo.IsName("IF_IDLE") && !stateInfo.IsName("IF_Andando"))
+        {
+            return;
+        }
+        anim.SetInteger("IFState", (int)InimigoState.Andando);
+        alvo = (posPlayer.position - transform.position).normalized;
+        if (alvo.x > 0)
+        {
+            direcaoAtual = 1f;
+            sr.flipX = false;
+        }
+        else if (alvo.x < 0)
+        {
+            direcaoAtual = -1f;
+            sr.flipX = true;
+        }
+        transform.position += (Vector3)(alvo * velocidade * Time.deltaTime);
     }
 }
